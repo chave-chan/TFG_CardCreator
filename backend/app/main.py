ea228df7a328c99ea014e7952e32a2a0d2362e35
chave-chan <aidachavero@gmail.com>
2024-11-14 08:10:36 +0100

    Started connection frontend and backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,6 +2,8 @@ import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 from app.api.v1.api_v1 import api_router
 from app.core.config import settings
 from app.database.session import engine, create_database
@@ -9,14 +11,22 @@ from app.database.base import Base
 
 app = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION)
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"], # Frontend URL
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Include the API routes
+app.include_router(api_router, prefix="/api/v1")
+
 Base.metadata.create_all(bind=engine)
 print("Tables created")
 
 # Create the database tables
 create_database()
-
-# Include the API routes
-app.include_router(api_router, prefix="/api/v1")
 
 # Define the root route as an example
 @app.get("/")
